caf272cad1a92a52ace9f3accf85fd2f7f02ef75
youngseo <ys550499@kaist.ac.kr>
2019-02-13 12:51:51 +0900

    Change toString method of WorkTime type

--- src/main/scala/scheduler/WorkTime.scala
+++ src/main/scala/scheduler/WorkTime.scala
@@ -10,22 +10,22 @@ abstract class WorkTime
 // Day work has end time. For example, Wednesday work is ended at 11:30
 case class Day(year: Int, month: Int, date: Int, day: DoW) extends WorkTime {
   override def toString: String = {
-    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일"
+    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일" + " 주간"
   }
 }
 case class Night(year: Int, month: Int, date: Int, day: DoW) extends WorkTime {
   override def toString: String = {
-    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일"
+    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일" + " 야간"
   }
 }
 // Off has two type, night work off and just off
 case class Off(year: Int, month: Int, date: Int, day: DoW) extends WorkTime { // Both night off and just off
   override def toString: String = {
-    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일"
+    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일" + " 비번"
   }
 }
 case class Holiday(year: Int, month: Int, date: Int, day: DoW) extends WorkTime {
   override def toString: String = {
-    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일"
+    year + "년 " + month + "월 " + date + "일 " + day.toString + "요일" + " 휴가"
   }
 }
